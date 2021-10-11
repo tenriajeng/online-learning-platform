4e20060371bc28dd289eb98c606d895dc43c28b4
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2021-10-11 23:41:54 +0800

    update config database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const knex = require("knex");
 const knexFile = require("../knexfile");
 
-const db = knex(knexFile.development);
+const db = knex(knexFile);
 
 module.exports = db;
--- knexfile.js
+++ knexfile.js
@@ -3,21 +3,18 @@ const path = require("path");
 db.config();
 
 module.exports = {
-	development: {
-		client: process.env.DB_CLIENT,
-		connection: {
-			host: process.env.DB_HOST,
-			user: process.env.DB_USER,
-			password: process.env.DB_PASSWORD,
-			database: process.env.DB_NAME,
-		},
-		migrations: {
-			directory: path.join(__dirname, process.env.MIGRATION_DIR),
-		},
-		seeds: {
-			directory: path.join(__dirname, process.env.SEEDS_DIR),
-		},
-		pool: { min: 0, max: 10 },
-	},
 	client: process.env.DB_CLIENT,
+	connection: {
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
+	},
+	migrations: {
+		directory: path.join(__dirname, process.env.MIGRATION_DIR),
+	},
+	seeds: {
+		directory: path.join(__dirname, process.env.SEEDS_DIR),
+	},
+	pool: { min: 0, max: 10 },
 };
