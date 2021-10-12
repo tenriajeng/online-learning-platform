4d70dcc41fb64b92c11ab9a7c956187e3933032e
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2021-10-12 10:05:09 +0800

    Update course.js
    add middleware to routes course admin service

--- routes/admin/course.js
+++ routes/admin/course.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const authMiddleware = require("../../middleware/auth");
 // const { multerUploads } = require("../../config/multer");
 const router = express.Router();
 const courseController = require("../../src/controller/admin/course.controller");
 const courseValidation = require("../../src/validation/admin/course.validation");
 
-router.get("/", courseController.courseList);
-router.post("/", courseValidation, courseController.courseCreate);
-router.put("/:courseId", courseValidation, courseController.courseUpdate);
-router.put("/destroy/:courseId", courseController.courseDestroy);
+router.get("/", authMiddleware, courseController.courseList);
+router.post("/", authMiddleware, courseValidation, courseController.courseCreate);
+router.put("/:courseId", authMiddleware, courseValidation, courseController.courseUpdate);
+router.put("/destroy/:courseId", authMiddleware, courseController.courseDestroy);
 
 module.exports = router;
