166b4ef5edb76810bdde0b14fe09dbe781f0dc01
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2021-10-12 11:55:49 +0800

    Update course.js
    add field category_id on course seeder

--- database/seed/course.js
+++ database/seed/course.js
@@ -5,11 +5,12 @@ exports.seed = async function (knex) {
 	await knex("course").del();
 
 	for (let index = 0; index <= 10; index++) {
-		await knex("course").insert(createCourseFaker());
+		await knex("course").insert(createCourseFaker(index));
 	}
 };
 
-const createCourseFaker = () => ({
+const createCourseFaker = (index) => ({
+	category_id: index + 1,
 	title: faker.lorem.word(),
 	slug: faker.lorem.slug(),
 	description: faker.lorem.text(),
